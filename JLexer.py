2e8b3912a657c9c47533fffc597b4e228e2d6a21
hoosierEE <ashroyer@gmail.com>
2014-11-19 21:49:46 -0500

    starting something

---
+++ JLexer.py
@@ -0,0 +1,26 @@
+"""
+    Pygments Lexer for J language
+"""
+
+import re
+from pygments.lexer import RegexLexer
+# from pygments.lexer import RegexLexer, bygroups, using, include
+from pygments.token import *
+# from pygments.token import Comment, Operator, Name, String, Number, Keyword
+
+__all__ = ['JLexer']
+
+class JLexer(RegexLexer):
+    name = 'J'
+    aliases = ['j']
+    filenames = ['*.ijs']
+
+    tokens = {
+        'root': [
+            (r'\s+', Text),
+            (r'NB\..*?\n', Comment.Single),
+            (r'.\s+Note.*?\n', Comment.Single),
+            (r'^Note.*?)', Comment.Multiline),
+        ]
+    }
+
