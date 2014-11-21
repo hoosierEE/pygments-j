ae556f6fef24c17d88cf672f68c303ce3c1f38bc
hoosierEE <ashroyer@gmail.com>
2014-11-21 08:12:13 -0500

    update lexer

--- j.py
+++ j.py
@@ -8,7 +8,7 @@ Lexer for the J programming language.
 
 import re
 from pygments.lexer import RegexLexer, words, include
-from pygments.token import Comment, Operator, Name, String, Number, Keyword, Text
+from pygments.token import Comment, Keyword, Name, Number, Operator, Punctuation, String, Text
 
 __all__ = ['JLexer']
 
@@ -28,13 +28,13 @@ class JLexer(RegexLexer):
             (r'[_0-9][_0-9.a-zA-Z]*', Number),
             (r'[a-z]\w+', Name),
             (r'[AcCeEiIjLopr]\.', Keyword),
-            (words(('if.', 'then.', 'else.', 'end.', 'while.', 'do.', 'echo')), Keyword),
+            (words(('if.', 'then.', 'else.', 'end.', 'while.', 'do.', 'echo')), Keyword.Reserved),
             (r'#!.*$', Comment.Preproc),
             (r'=[.:]', Keyword.Declaration),
             (r'[~!@#$%^&*+-=;:"{}\[\]<>\?]', Operator),
             (r'NB\..*?\n', Comment.Single),
             (r'Note.*', Comment.Multiline, 'comment'),
-            (r'\(', Keyword, 'parentheses'),
+            (r'\(', Punctuation, 'parentheses'),
             (r"'", String, 'singlequote'),
             # (r'(?s).', Text), # uncomment when this lexer is complete
         ],
@@ -44,7 +44,7 @@ class JLexer(RegexLexer):
             (r'^\)', Comment.Multiline, '#pop'),
         ],
         'parentheses': [
-            (r'\)', Keyword, '#pop'),
+            (r'\)', Punctuation, '#pop'),
             include('root'),
         ],
         'singlequote': [
